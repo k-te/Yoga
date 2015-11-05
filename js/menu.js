00b368d9f1e00bdcec7b2abdc14443a59375c508
K-Te <apsadykova@yandex.ru>
2015-11-05 23:07:31 +0300

    project

---
+++ js/menu.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+        var $menu = $("#menu");
+
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 100 && $menu.hasClass("default") ){
+                $menu.fadeOut('slow',function(){
+                    $(this).removeClass("default")
+                           .addClass("fixed transbg")
+                           .fadeIn('slow');
+                });
+            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("fixed transbg")
+                           .addClass("default")
+                           .fadeIn('slow');
+                });
+            }
+        });//scroll
+
+        $menu.hover(
+            function(){
+                if( $(this).hasClass('fixed') ){
+                    $(this).removeClass('transbg');
+                }
+            },
+            function(){
+                if( $(this).hasClass('fixed') ){
+                    $(this).addClass('transbg');
+                }
+            });//hover
+    });//jQuery
---
+++ js/slider.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.slider1').bxSlider({
+    slideWidth: 350,
+    minSlides: 1,
+    maxSlides: 1,
+    moveSlides: 1,
+    // slideMargin: 10
+  });
+  $('.slider2').bxSlider({
+   
+    minSlides: 1,
+    maxSlides: 1,
+    moveSlides: 1,
+    // slideMargin: 10
+  });
+});
